e5a68648d6662781710efe0599022820a40f41c2
Your Name <you@example.com>
2021-05-16 00:01:49 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+//mod bindings {
+//    windows::include_bindings!();
+//}
+
+use bindings::{
+    Windows::Data::Xml::Dom::*,
+    Windows::Win32::System::Threading::{CreateEventW, SetEvent, WaitForSingleObject},
+    Windows::Win32::System::WindowsProgramming::CloseHandle,
+    Windows::Win32::UI::WindowsAndMessaging::{MessageBoxA, MB_OK},
+};
+
+fn main() -> windows::Result<()> {
+    let doc = XmlDocument::new()?;
+    doc.LoadXml("<html>hello world</html>")?;
+
+    let root = doc.DocumentElement()?;
+    assert!(root.NodeName()? == "html");
+    assert!(root.InnerText()? == "hello world");
+
+    unsafe {
+        let event = CreateEventW(std::ptr::null_mut(), true, false, None);
+        SetEvent(event).ok()?;
+        WaitForSingleObject(event, 0);
+        CloseHandle(event).ok()?;
+
+        MessageBoxA(None, "Text", "Caption", MB_OK);
+    }
+
+    Ok(())
+}
